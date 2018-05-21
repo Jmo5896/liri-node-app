089cc1068d4f880d1ef6b329acaaad839dfe1b31
Jmo5896 <jmo5896@moore-clan.net>
2018-05-20 22:56:32 -0600

    3rd commit

--- liri.js
+++ liri.js
@@ -43,6 +43,8 @@ var client = new Twitter(myKeys.twitter);
 
 //choose a command:
 var inquirer = require('inquirer');  //npm install inquirer
+var fs = require('fs');
+var request = require('request');
 
 inquirer.prompt([
     {
@@ -53,24 +55,116 @@ inquirer.prompt([
     }
 ]).then(function(inquirerResponse) {
     //  console.log('you chose ' + inquirerResponse.command);
-    
-    if (inquirerResponse.command === 'my-tweets') { //npm install twitter
-        console.log('welcome to ' + inquirerResponse.command);
-        var params = {screen_name: 'nodejs'};
-        client.get('favorites/list', function(error, tweets, response) {
-            if(error) throw error;
-            console.log(tweets);  // The favorites.
-            console.log(response);  // Raw response object.
-        });
-    } else if (inquirerResponse.command === 'spotify-this-song') { //npm install node-spotify-api
-        console.log('welcome to ' + inquirerResponse.command);
-    } else if (inquirerResponse.command === 'movie-this') { //npm install request & npm install 
-        console.log('welcome to ' + inquirerResponse.command);
-    } else if (inquirerResponse.command === 'do-what-it-says') {
-        console.log('welcome to ' + inquirerResponse.command);        
-    }
+    makeDecision(inquirerResponse.command);
+
 });
 
+function makeDecision(command) {
+    switch (command) {
+        case 'my-tweets':
+            getTweets();
+            break;
+        
+        case 'do-what-it-says':
+            getRandom();
+            break; 
+        default:
+        promptSearchTerm(command);
+    }
+}
 
+function promptSearchTerm (command) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'searchTerm',
+            message: 'What do you want to search?'
+            
+        }
+    ]).then(function(inquirerResponse) {
+        var searchTerm = inquirerResponse.searchTerm;
+        switch (command) {
+        case 'spotify-this-song':
+            getSpotify(searchTerm);
+            break;
+        case 'movie-this':
+            getMovie(searchTerm);
+            break;
+        }
+    });
+}
+
+function getTweets () {
+    var params = {screen_name: 'Jmo5896'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log(tweets[0]);
+            var myTweets = [];
+            for (var i = 0; i < tweets.length; i++) {
+                var myTweet = {
+                    text: tweets[i].text,
+                    created: tweets[i].created_at
+                }
+                myTweets.push(myTweet);
+            }
+            console.log(myTweets);
+            log(myTweets);
+        } 
+    });
+}
+function getSpotify(songName) {
+    if (songName === '' || !songName) {
+        songName = 'The Sign Ace of Base';
+    }
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+        var songData = data.tracks.items[0];
+        var song = {
+            artists: songData.artists[0].name,
+            track: songData.name,
+            album: songData.album.name,
+            link: songData.preview_url
+        }
+        console.log(song); 
+        log(song);
+    });  
+}
+function getMovie(movieName) {
+    if (movieName === '' || !movieName) {
+        movieName = 'Mr. Nobody.';
+    }
+    request('http://www.omdbapi.com/?apikey=trilogy&t=' + movieName + '&y=&plot=full&tomatoes=true', function (error, response, body) {
+        var movieData = JSON.parse(body);
+        var movie = {
+            title: movieData.Title,
+            rated: movieData.Rated,
+            released: movieData.Year,
+            country: movieData.Country,
+            language: movieData.Language,
+            tomatoes: movieData.Ratings[1].Value,
+            plot: movieData.Plot,
+            actors: movieData.Actors,
+            imdbRating: movieData.imdbRating
+        }
+        console.log(movie);
+        log(movie);
+    });   
+}
+function getRandom () {
+
+    fs.readFile('./random.txt', 'utf8', function(error, data) {
+        var randomArr = data.split(', ');
+        getSpotify(randomArr[1]);
+    });
+}
+function log(data) {
+    fs.appendFile('./log.txt', JSON.stringify(data) + '\n', function(err) {
+        console.log('data was logged');
+
+    });
+}
 
 
