cc7724548fbbd4e9d763de2e4b635199dee9b012
Jmo5896 <jmo5896@moore-clan.net>
2018-05-20 23:00:25 -0600

    4th commit

--- liri.js
+++ liri.js
@@ -5,46 +5,13 @@ var myKeys = require('./keys.js');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 //access key information
-var spotify = new Spotify(myKeys.spotify);
-var client = new Twitter(myKeys.twitter);
-
-//================================================================
-// Make it so liri.js can take in one of the following commands:
-    // * `my-tweets`
-    // * `spotify-this-song`
-    // * `movie-this`
-    // * `do-what-it-says`
-//What each command should do:
-    // node liri.js my-tweets
-        // This will show your last 20 tweets and when they were created at in your terminal/bash window.
-    // node liri.js spotify-this-song '<song name here>'
-        // This will show the following information about the song in your terminal/bash window:
-            // Artist(s)
-            // The song's name
-            // A preview link of the song from Spotify
-            // The album that the song is from
-        // If no song is provided then your program will default to "The Sign" by Ace of Base.
-    // node liri.js movie-this '<movie name here>'
-        // This will output the following information to your terminal/bash window:
-            // * Title of the movie.
-            // * Year the movie came out.
-            // * IMDB Rating of the movie.
-            // * Rotten Tomatoes Rating of the movie.
-            // * Country where the movie was produced.
-            // * Language of the movie.
-            // * Plot of the movie.
-            // * Actors in the movie.
-        // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-    // node liri.js do-what-it-says
-        // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-        // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-        // Feel free to change the text in that document to test out the feature for other commands.
-//================================================================
+var spotify = new Spotify(myKeys.spotify); //npm install node-spotify-api
+var client = new Twitter(myKeys.twitter); //npm install twitter
 
 //choose a command:
 var inquirer = require('inquirer');  //npm install inquirer
 var fs = require('fs');
-var request = require('request');
+var request = require('request'); //npm install request
 
 inquirer.prompt([
     {
